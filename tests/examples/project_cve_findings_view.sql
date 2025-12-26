a7b904d4889544d2b20595f0e1ac97dbe68a415d
Zhiwen <tzhizhiwenwen@gmail.com>
2025-12-26 16:07:15 +0800

    update

--- tests/examples/project_cve_findings_view.sql
+++ tests/examples/project_cve_findings_view.sql
@@ -1,13 +1,13 @@
 CREATE OR REPLACE VIEW project_cve_findings_view AS
 SELECT
-  cf.id        AS cve_finding_id,
   p.project,
   i.path AS image_path,
   i.tag AS image_tag,
   cf.cve_id,
   cf.package_name,
   cf.severity,
-  cf.justified
+  cf.justified,
+  cf.id        AS cve_finding_id
 FROM cve_findings cf
 JOIN images i
   ON cf.image_id = i.id
